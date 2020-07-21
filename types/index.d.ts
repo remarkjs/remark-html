12448ae26c3e89b054c97c6ab6156347a5e44773
TaylorBeeston <beeston.taylor@gmail.com>
2020-07-20 19:44:35 -0700

    Update type definition to include sanitize and handlers

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,9 +2,19 @@
 
 import {Plugin} from 'unified'
 import {HastUtilToHtmlOptions} from 'hast-util-to-html'
+import {Schema} from 'hast-util-sanitize'
+import {Handlers} from 'mdast-util-to-hast'
 
 interface htmlOptions extends HastUtilToHtmlOptions {
-  sanitize?: boolean | {[key: string]: any}
+  /**
+   * How to sanitize the output
+   */
+  sanitize?: boolean | Schema
+
+  /**
+   * Object mapping mdast nodes to functions handling them
+   */
+  handlers?: Handlers
 }
 
 declare const html: Plugin<[htmlOptions?]>
